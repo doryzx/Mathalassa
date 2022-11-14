ea8232c424e11f5d97632214eba8ec9c20f637f3
LeanneLuo <leanne.luo8@gmail.com>
2022-11-14 11:18:46 -0500

    more popup progress

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const element = document.getElementById("open");
-element.addEventListener("click", myFunction);
-
 function myFunction() {
-  document.getElementById("aboutText").innerHTML = "This game is about....";
-}
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+  }
