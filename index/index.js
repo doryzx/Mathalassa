de44efffd348fb583e99e4be8a2840d6edb4dfd3
LeanneLuo <leanne.luo8@gmail.com>
2022-11-18 10:13:19 -0500

    added to popup

--- index/index.js
+++ index/index.js
@@ -1,4 +1,7 @@
-function myFunction() {
-    var popup = document.getElementById("myPopup");
+function theFunction() {
+    var popup = document.getElementById("thePopup");
     popup.classList.toggle("show");
   }
+
+//The popup does not necessarily needs a "close button", an easier solution is just simply use the entities around the popup to close pop up
+//Or simply use the popup itself, with message below as "click again to close" or "skip", if that is a tutorial-type popup. 
