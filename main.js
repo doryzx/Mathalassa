0048894e6ef3997d95c02d3a3cfd378866ff48c5
yrodhacker <catfishbadfish@gmail.com>
2022-12-29 14:30:50 -0500

    we are literate

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-const fs = require('fs')
-var inpu = "hello"
-fs.writeFile('data.txt', , (err)=>{
-    if(err) throw err;
-    else{
-        console.log("oui oui baguette")
-    }
-    }
-)
+const fs = require('fs')//import read/write module
+var logger = fs.createWriteStream('data.txt')
+var writeLine = (line) => logger.write(`\n${line}`);
+  writeLine('Data written to a new me');
+  writeLine("fishdoesart was here");
+  logger.end() // close string
+
+  fs.readFile('data.txt', function (err, data) {
+    console.log("Asynchronous read: " + data.toString());
+ });
