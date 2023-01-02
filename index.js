68dd5c4075759c50bf7e1a43ea905a7ab63b0050
yrodhacker <catfishbadfish@gmail.com>
2023-01-02 14:06:39 -0500

    save

--- index.js
+++ index.js
@@ -12,6 +12,8 @@ app.get('/', (req, res) => {        //get requests to the root ("/") will route 
 app.listen(port, () => {            //server starts listening for any attempts from a client to connect at port: {port}
     console.log(`Now listening on port ${port}`); 
 });
+var path = require('path')
+app.use(express.static(path.join(__dirname, '/')));
 
 function theFunctionAbout() {
     document.getElementById("thePopupAbout").style.visibility="visible";
