dd4f115ef392014851290df1c5fae5f380499036
yrodhacker <catfishbadfish@gmail.com>
2023-01-14 19:50:19 -0500

    more question generating

--- questions/questions.js
+++ questions/questions.js
@@ -28,29 +28,46 @@ var lives=3;
 var questions=5;//questions they need to answer correctly
 var response;
 var tries=2;
+var finished=false;
+let things=//15 things
+    ["apples","bananas","oranges","trees","candies",
+    "cookies","legos","clarinets","scoopulas","chocolates",
+    "chickens","clarinets","phones","bowls","plants"];
+let names=//20 names
+    ["Bob","Kirsten","Xiyue","Jina","Joe","Dory","Leanne","Chris","Alex","Alexis",
+    "Ellie","Calvin","Josh","Emily","Curtis","Ahmed","Camille","Siobhan","Kevin","Maria"];
 const level=getLevelCurrent();
-var answer = generate(level);
+var answer;
+generate(level);
 //wait for them to call setResponse
 function setResponse(){//checks if the response is correct and shows hints/text based on try count, or lets them move on to the next q
     event.preventDefault();
     response = document.getElementById("questionInput").value;
+    if(finished==false){
     if(response==answer){
         questions--;
+        finished=true;
+        document.getElementById("message").style.color="green";
         document.getElementById("message").innerHTML="Correct! Let's move on to the next question";
         document.getElementById("message").style.visibility="visible";
         document.getElementById("fishTextAct").innerHTML="HOORAY! One step closer to freedom";
         document.getElementById("nextQuestion").style.visibility="visible";
     }
-    if(response!=answer){
+    else if(response!=answer){
         tries--;
         if(tries==1){
             document.getElementById("hint").style.visibility="visible";
+            document.getElementById("message").style.color="red";
             document.getElementById("message").innerHTML="Incorrect. Try Again";
+            document.getElementById("message").style.visibility="visible";
             document.getElementById("fishTextAct").innerHTML="Oh no! Be Careful!"
         }
         else if(tries==0){
             lives--;
+            finished=true;
             document.getElementById("message").innerHTML="Incorrect. You lost a life";
+            document.getElementById("message").style.color="red";
+            document.getElementById("message").style.visibility="visible";
             document.getElementById("fishTextAct").innerHTML="AHHH! We lost a life, I'm scared!";
             document.getElementById("nextQuestion").style.visibility="visible";
             if(lives==2){
@@ -63,6 +80,7 @@ function setResponse(){//checks if the response is correct and shows hints/text 
                 document.getElementById("life1").style.visibility="hidden";
             }
         }
+    }
        
     }
 }
@@ -72,37 +90,74 @@ function checkEnd(){//checks if they've failed or passed
     }
     else if(questions==0){
         document.getElementById("levelPassed").style.visibility="visible";
+        if(getLevelCurrent()>getLevelCompleted()){
+            setLevelCompleted(getLevelCurrent);
+        }
     }
 }
 function nextQuestion(){
     checkEnd();
+    finished=false;
     document.getElementById("nextQuestion").style.visibility="hidden";
     document.getElementById("message").style.visibility="hidden";
+    document.getElementById("hint").style.visibility="hidden";
     document.getElementById("fishTextAct").innerHTML="You've got this! This is easy-peasy";
     tries=2;
-    answer= generate(level);
+    generate(level);
 }
-
-let bank=
-    ["apples","bananas","oranges","trees","candies",
-    "cookies","legos","clarinets","scoopulas","chocolates",
-    "chickens","Kellens","phones","bowls","plants"];
+    
 function generate(level){//this sets the question text, hint text, and returns the answer
-    var answer;
     var question;
     var hint;
     var a;
     var b;
+    var thing=things[generateNum(0,14)];
+    var name=names[generateNum(0,19)];
     switch(level){
         case 1: 
+            a=generateNum(2,5);
+            console.log(a);
+            b=generateNum(2,5);
+            answer=a+b;
+            question=("You have "+a+" "+thing+", and your friend "+name
+            +" gives you "+b+" "+thing+". How many do you have in total?");
+            hint="Hint: Try "+a+" + "+b;
             break;
         case 2: 
+            a=generateNum(5,20);
+            console.log(a);
+            b=generateNum(6,20);
+            answer=a+b;
+            question=("You have "+a+" "+thing+", and your friend "+name
+            +" gives you "+b+" "+thing+". How many do you have in total?");
+            hint="Hint: Try "+a+" + "+b+", don't forget to carry when needed";
             break;
         case 3: 
+            a=generateNum(3,9);
+            console.log(a);
+            b=generateNum(2,a);
+            answer=a-b;
+            question=("You had "+a+" "+thing+", and your friend "+name
+            +" takes away "+b+" "+thing+". How many do you have in total now?");
+            hint="Hint: Try "+a+" - "+b;
            break; 
-        case 4: 
+        case 4:
+            a=generateNum(3,19);
+            console.log(a);
+            b=generateNum(2,a);
+            answer=a-b;
+            question=("You had "+a+" "+thing+", and your friend "+name
+            +" takes away "+b+" "+thing+". How many do you have in total now?");
+            hint="Hint: Try "+a+" - "+b+", don't forget to borrow when needed"; 
            break; 
         case 5: 
+            a=generateNum(3,19);
+            console.log(a);
+            b=generateNum(2,a);
+            answer=a-b;
+            question=("You had "+a+" "+thing+", and your friend "+name
+            +" takes away "+b+" "+thing+". How many do you have in total now?");
+            hint="Hint: Try "+a+" - "+b+", don't forget to borrow when needed"; 
             break;
         case 6: 
             break;
@@ -115,9 +170,8 @@ function generate(level){//this sets the question text, hint text, and returns t
     }
     document.getElementById("question").innerHTML=question;
     document.getElementById("hint").innerHTML=hint;
-    return answer;
 }
 
-function generate(min, max){
+function generateNum(min, max){
    return(Math.floor(Math.random() * (max - min + 1) + min));
 }
